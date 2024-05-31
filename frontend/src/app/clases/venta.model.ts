5571661a85f816fca56df4ed47b4b9473882c683
Nikill85 <nilda341985@hotmail.com>
2024-05-30 21:19:07 -0300

    Diseño

--- frontend/src/app/clases/venta.model.ts
+++ frontend/src/app/clases/venta.model.ts
@@ -1,7 +1,7 @@
 export class Venta {
      id_ventas: number | undefined
      fecha_venta: Date | undefined;
-     fk_producto: number | undefined;
+     fk_producto: any | undefined;
      cantidad: number | undefined;
      total_venta: number | undefined;
     
