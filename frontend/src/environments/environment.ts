850bf1b7a48fc6f1351b9d06fce01dac6ef5a0a6
Nikill85 <nilda341985@hotmail.com>
2024-07-26 06:36:48 -0300

    seguimos

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
     production: false,
-    apiUrl: 'https://control-stock-juguetes-1.onrender.com' // URL de tu backend en Render
+    apiUrl:'http://localhost:3000/proveedor' // URL de tu backend en Render
   };
