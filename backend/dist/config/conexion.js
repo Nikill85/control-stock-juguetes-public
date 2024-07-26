debec0ace6a487670468104307add335fc10d514
Nikill85 <nilda341985@hotmail.com>
2024-07-26 03:56:26 -0300

    BD

--- backend/dist/config/conexion.js
+++ backend/dist/config/conexion.js
@@ -1,17 +1,10 @@
-// "use strict";
-// var __importDefault = (this && this.__importDefault) || function (mod) {
-//     return (mod && mod.__esModule) ? mod : { "default": mod };
-// };
-// Object.defineProperty(exports, "__esModule", { value: true });
-// const promise_1 = __importDefault(require("mysql2/promise"));
-// var conexion = promise_1.default.createPool({
-//     host: 'localhost',
-//     port: 3306,
-//     user: 'root',
-//     password: 'yduz2urogsgovg',
-//     database: 'proyecto_final'
-// });
-// exports.default = conexion;
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const promise_1 = __importDefault(require("mysql2/promise"));
+
 
 require('dotenv').config();
 const mysql = require('mysql2');
